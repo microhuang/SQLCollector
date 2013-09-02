3f6e02f9292de44212fc1c7c15505eb7b87c2dc4
microhuang <microhuang@live.com>
2013-09-02 12:35:29 +0800

    Create admin.lua

---
+++ admin.lua
@@ -0,0 +1,13 @@
+	elseif query:lower() == "select * from clientinfo" then
+		fields = {{name="id",type=proxy.MYSQL_TYPE_LONG},{name="client",type=proxy.MYSQL_TYPE_STRING},{name="description",type=proxy.MYSQL_TYPE_STRING}}
+		--rows[#rows + 1] = { 1, proxy.global.clientinfo[1] }
+		--for i = 1, #proxy.global.clientinfo do
+		--	rows[#rows + 1] = {
+		--		i,
+		--		proxy.global.clientinfo[i]
+		--	}
+		for k,v in pairs(proxy.global.clientinfo) do
+			rows[#rows + 1] = {
+			    i,k,v
+			}
+		end
