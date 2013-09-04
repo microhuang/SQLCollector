a4f2f51094e6a1563249139760ac4754f545e77f
microhuang <microhuang@live.com>
2013-09-04 09:57:57 +0800

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -1,5 +1,5 @@
 	elseif query:lower() == "select * from clientinfo" then
-		fields = {{name="id",type=proxy.MYSQL_TYPE_LONG},{name="client",type=proxy.MYSQL_TYPE_STRING},{name="description",type=proxy.MYSQL_TYPE_STRING}}
+		fields = {{name="id",type=proxy.MYSQL_TYPE_LONG},{name="client",type=proxy.MYSQL_TYPE_STRING},{name="description",type=proxy.MYSQL_TYPE_STRING}, {name="time",type=proxy.MYSQL_TYPE_LONG}}
 		--rows[#rows + 1] = { 1, proxy.global.clientinfo[1] }
 		--for i = 1, #proxy.global.clientinfo do
 		--	rows[#rows + 1] = {
@@ -8,6 +8,6 @@
 		--	}
 		for k,v in pairs(proxy.global.clientinfo) do
 			rows[#rows + 1] = {
-			    i,k,v
+			    i,k,v,os.time()
 			}
 		end
